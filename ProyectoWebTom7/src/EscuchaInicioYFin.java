07bd9a1d729827d93130883b533a75a9ff0ac991
alvgarfer <alvgarfer@gmail.com>
2017-02-14 16:09:33 +0100

    update

--- ProyectoWebTom7/src/EscuchaInicioYFin.java
+++ ProyectoWebTom7/src/EscuchaInicioYFin.java
@@ -6,38 +6,34 @@ import javax.servlet.annotation.WebListener;
 
 /**
  * Application Lifecycle Listener implementation class EscuchaInicioYFin
- *
  */
 @WebListener
-public class EscuchaInicioYFin implements ServletContextListener {
-
-    /**
-     * Default constructor. 
-     */
-    public EscuchaInicioYFin() {
-        // TODO Auto-generated constructor stub
+public class EscuchaInicioYFin implements ServletContextListener
+{  
+	/**Default constructor. */
+    public EscuchaInicioYFin()
+    {
+    	
     }
 
 	/**
      * @see ServletContextListener#contextDestroyed(ServletContextEvent)
      */
-    public void contextDestroyed(ServletContextEvent arg0)  { 
-         // TODO Auto-generated method stub
-    	System.out.println("PROGRMA DESTRUIDO");
-    	System.out.println("PROGRMA DESTRUIDO");
-    	System.out.println("PROGRMA DESTRUIDO");
-    	System.out.println("PROGRMA DESTRUIDO");
+    public void contextDestroyed(ServletContextEvent arg0) 
+    {
+    	System.out.println("PROGRAMA DESTRUIDO");
+    	System.out.println("PROGRAMA DESTRUIDO");
+    	System.out.println("PROGRAMA DESTRUIDO");
+    	System.out.println("PROGRAMA DESTRUIDO");
     }
 
-	/**
-     * @see ServletContextListener#contextInitialized(ServletContextEvent)
-     */
-    public void contextInitialized(ServletContextEvent arg0)  { 
-         // TODO Auto-generated method stub
-    	System.out.println("PROGRMA INICIADO");
-    	System.out.println("PROGRMA INICIADO");
-    	System.out.println("PROGRMA INICIADO");
-    	System.out.println("PROGRMA INICIADO");
+	/** @see ServletContextListener#contextInitialized(ServletContextEvent)*/
+    public void contextInitialized(ServletContextEvent arg0)
+    { 
+    	System.out.println("PROGRAMA INICIADO");
+    	System.out.println("PROGRAMA INICIADO");
+    	System.out.println("PROGRAMA INICIADO");
+    	System.out.println("PROGRAMA INICIADO");
     	
     }
 	
